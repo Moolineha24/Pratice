39bd2d06a2a379838522586b800db90e0a616882
Mooli Neha <moolineha@gmail.com>
2025-07-01 12:29:36 +0530

    function changed

---
+++ class1.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b !== 0) {
+    return a / b;
+  } else {
+    return "Cannot divide by zero!";
+  }
+}
+
+// Example usage
+console.log("Add: " + add(10, 5));
+console.log("Subtract: " + subtract(10, 5));
+console.log("Multiply: " + multiply(10, 5));
+console.log("Divide: " + divide(10, 5));
